13c11692a2f4f74f0556ee031c59346bf5e0f3db
Hieu <htn619@gmail.com>
2022-06-20 00:57:13 -0700

    backend is done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
 require('dotenv').config();
-const { PORT = 3000, DATABASE_URL } = process.env
+const { PORT = 3001, DATABASE_URL } = process.env
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
